015db783d22c03f7f194b7a415f15451bb75fa07
rafgger <rafgger@gmail.com>
2025-05-06 16:10:48 +0200

    writing into the database | checking for duplicity

--- 9.1+Authentication+Lv.1/index.js
+++ 9.1+Authentication+Lv.1/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "password",
+  post: 5432
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -21,9 +32,30 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  console.log(req.body.username);
-  console.log(req.body.password);
-  
+  // console.log(req.body.username);
+  // console.log(req.body.password);
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    }
+    {
+    const result = await db.query(
+      "INSERT INTO users (email, password) VALUES ($1, $2)",
+      [email, password]
+    );
+    console.log(result);
+    res.render("secrets.ejs");
+  }
+} catch (err) {
+  console.log(err);
+}
 });
 
 app.post("/login", async (req, res) => {
